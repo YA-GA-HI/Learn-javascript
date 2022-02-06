375dbcb8b3a7d100eebf6fb1d351355cb920fd75
Zakariaa Enajjachi <72130366+YA-GA-HI@users.noreply.github.com>
2022-02-06 04:02:10 +0100

    Create EscapeSequence.js

---
+++ EscapeSequence.js
@@ -0,0 +1,43 @@
+/*
+\n new line
+
+\' print ' inside ''
+the same for "
+
+\\ print \
+
+\ escape new line
+*/
+
+
+//concat
+/*
+
+concat with +
+*/
+
+
+//template litterals
+
+/*
+
+`
+${}
+`
+
+*/
+
+
+/*
+
+challenges
+
+let Title = "Zenku" , Description = "Zenku is here" , Date = "06/02";
+let html = `
+<h3>${ Title }</h3>
+<p>${ Description }</p>
+<span>${ Date }</span>
+`;
+
+document.write(html);
+*/
