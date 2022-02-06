56be09d9d9ba0b6946b40e704000f1d89aeb41b7
Zakariaa Enajjachi <72130366+YA-GA-HI@users.noreply.github.com>
2022-02-06 19:05:56 +0100

    string

---
+++ number.js
@@ -0,0 +1,41 @@
+console.log( (100).toString() );
+
+console.log( 100..toString() );
+
+
+console.log( (100.555).toFixed() );
+/*
+length of numbers to return after .
+a string
+*/
+
+console.log( parseInt("000") )
+
+console.log( parseFloat("000.67") )
+
+
+console.log( isInteger(parseInt("000"))  );
+
+
+console.log( isNaN("000")  );
+
+
+/*
+
+Math.
+
+min( nm1, nm2 )
+
+min( nm1, nm2 )
+
+ceil(  )
+
+floor(  )
+
+random()
+
+pow( 2, x );
+
+trunc( 99.5 );
+//return only integer part
+*/
