4210778eb7c16256c72dcf92e4a17ea9308aa878
Zakariaa Enajjachi <72130366+YA-GA-HI@users.noreply.github.com>
2022-02-06 03:36:18 +0100

    Update Var.js

--- Var.js
+++ Var.js
@@ -1,1 +1,20 @@
+let x = 5;
+//let & var is declaring a new variable
+// x is the unique name for variable
+// 5 is the variable value
 
+let x = 5 , y = 5;
+
+/*
+note:
+if html has element with id = "something"
+we can access it in js by;
+something.innerHTML for example:
+*/
+
+
+
+/*
+js is loosly type
+it's not required to specify the type of var
+*/
