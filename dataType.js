f6b674f75e2ecd6c90daf802c2294a4b9f8f478c
Zakariaa Enajjachi <72130366+YA-GA-HI@users.noreply.github.com>
2022-02-06 03:28:18 +0100

    Create dataType.js
    data type file created

---
+++ dataType.js
@@ -0,0 +1,18 @@
+console.log( typeof "zenkuu" );
+//string
+
+
+console.log( typeof 30.09 );
+//number
+
+console.log( typeof [2,3,4] );
+//array => object
+
+console.log( typeof {name:"zenkuu", age:17 } );
+//object
+
+console.log( typeof 2 === 3 );
+//boolean
+
+console.log( typeof undefined );
+//undefined
