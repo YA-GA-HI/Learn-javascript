992d5ec01a77ccbe75345e3bc0160e052e111e63
Zakariaa Enajjachi <72130366+YA-GA-HI@users.noreply.github.com>
2022-02-07 03:05:45 +0100

    Create Str.js

---
+++ Str.js
@@ -0,0 +1,16 @@
+let str = "heelo world";
+
+//str[0] === str.charAt(0)
+//str.length
+//trim ltrim rtrim
+//toUpperCase & toLowerCase
+//indexOf( str , start index )
+//lastIndexOf( str , start length )
+//str.slice(start , end  )
+//str.repeat( 5 )
+//str.split( separator , limit )
+//divise str o array using separator
+//substring( start , end )
+//str.includes("heelo")
+//startsWith()
+//endWidth()
